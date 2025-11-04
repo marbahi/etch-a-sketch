588048cd4cf31237fbadc482682754852f7baabd
marbahi <ammar.skyagt@gmail.com>
2025-11-04 17:49:36 +0700

    Adding button
    I add reset button to change how many grid, i transfrom the looping to make grid into function
    for hover maybe still not clean code, and i cahnge border of grid to gray from black

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-let manyGrid = 16;
+let defaultManyGrid = 16;
+let changedManyGrid = 0;
 const sizeContainer = 800;
 
 const containers = document.getElementById("container");
 
-let fixedSizeGrid = (sizeContainer / manyGrid) - 2;
+function makeGrid (manyGrid) {
+    let fixedSizeGrid = 0;
+    fixedSizeGrid = (sizeContainer / manyGrid) - 2;
 
-for (i = 0; i < manyGrid; i++){
-    for (j = 0; j < manyGrid; j++){
-        const grid = document.createElement("div");
-        grid.classList.add("Grid");
-        grid.style.width = fixedSizeGrid + 'px';
-        grid.style.height = fixedSizeGrid + 'px';
-        grid.style.backgroundColor = 'white';
+    for (i = 0; i < manyGrid; i++){
+        for (j = 0; j < manyGrid; j++){
+            const grid = document.createElement("div");
+            grid.classList.add("Grid");
+            grid.style.width = fixedSizeGrid + 'px';
+            grid.style.height = fixedSizeGrid + 'px';
+            grid.style.backgroundColor = 'white';
 
-        containers.appendChild(grid);
+            containers.appendChild(grid);
+        }
     }
 }
+
+
+// Starting Grid
+const startGrid = makeGrid(defaultManyGrid);
 
 let hoverGrid = document.querySelectorAll(".Grid");
 hoverGrid.forEach(grid => {
@@ -25,4 +33,27 @@ hoverGrid.forEach(grid => {
     grid.addEventListener("mouseleave", () => {
         grid.style.backgroundColor = 'lightblue';
     });
+})
+
+// Changed Grid
+const resetBtn = document.querySelector("#btn");
+resetBtn.addEventListener("click", () => {
+    changedManyGrid = parseInt(prompt("Reset the grid (16 - 100)"));
+    const removeGrid = document.querySelectorAll(".Grid");
+
+    removeGrid.forEach(grid => {
+        grid.remove()
+    })
+
+    const resetGrid = makeGrid(changedManyGrid);
+
+    let hoverGrid = document.querySelectorAll(".Grid");
+    hoverGrid.forEach(grid => {
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = 'lightblue';
+        });
+        grid.addEventListener("mouseleave", () => {
+            grid.style.backgroundColor = 'lightblue';
+        });
+    })
 })
